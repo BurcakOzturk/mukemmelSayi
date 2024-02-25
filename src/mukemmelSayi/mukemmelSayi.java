bbf80d1b17cd3437c1d10ea00612b58d4d5a28aa
Burçak ÖZTÜRK <159647466+BurcakOzturk@users.noreply.github.com>
2024-02-26 00:16:55 +0300

    ilk yayim

---
+++ src/mukemmelSayi/mukemmelSayi.java
@@ -0,0 +1,25 @@
+package mukemmelSayi;
+
+import java.util.Scanner;
+
+public class mukemmelSayi {
+
+	public static void main(String[] args) {
+
+		int sayi,i,toplam;		
+		Scanner input = new Scanner(System.in);
+
+		while (true) {
+			
+			toplam=0;
+			System.out.print("Bir sayı giriniz: ");
+			sayi = input.nextInt();
+						
+			for (i=1 ; i<sayi ; i++) {
+				if(sayi%i==0) toplam=toplam+i; 
+			}
+			if (toplam==sayi) System.out.println(sayi+" Mükemmel sayıdır.\n\n");
+			else System.out.println(sayi+" Mükemmel sayi değildir.\n\n");		
+		}
+	}
+}
